66e07aae9334ce25c92556edf53c92f272465589
Valeria Karimova <valeriakarimova2912@gmail.com>
2022-03-22 20:04:29 +0300

    feat: create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { CardComponent } from './card/card.component';
+import { SearchComponent } from './search/search.component';
+import { SortingComponent } from './sorting/sorting.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    CardComponent,
+    SearchComponent,
+    SortingComponent,
   ],
   imports: [
     BrowserModule,
